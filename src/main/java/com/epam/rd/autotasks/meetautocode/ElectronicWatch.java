66d8f857c7823aae20922d5713387c5238385df7
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-04-28 10:28:08 +0300

    12/12 passed

--- src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -10,9 +10,9 @@ public class ElectronicWatch {
         int minutes = (seconds - hours*3600)/60;
         seconds = seconds - (hours*3600 + minutes*60);
         if (hours >= 24) hours = 0;
-        if (minutes / 10 == 0 || minutes == 0) {
+        if (minutes / 10 == 0 && seconds / 10 != 0) {
             System.out.println(hours + ":0" + minutes + ":" + seconds);
-        } else if (seconds / 10 == 0 || seconds == 0) {
+        } else if (seconds / 10 == 0 && minutes / 10 != 0) {
             System.out.println(hours + ":" + minutes + ":0" + seconds);
         } else if (minutes / 10 == 0 && seconds / 10 == 0) {
             System.out.println(hours + ":0" + minutes + ":0" + seconds);
